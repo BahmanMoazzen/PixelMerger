44b0f6bdb928311def44916c5957f7bce90d857c
Bahman Moazzen <bahmanmoazzen@gmail.com>
2026-06-03 15:12:48 +0930

    Claim Buttons Inserted

--- Assets/PixelMerger/Scripts/AftermathManager.cs
+++ Assets/PixelMerger/Scripts/AftermathManager.cs
@@ -4,11 +4,24 @@ using UnityEngine.UI;
 
 public class AftermathManager : MonoBehaviour
 {
-
+    /// <summary>
+    /// the best ever score of the player, the score of this run, and the total score of the player are shown in the aftermath screen. these scores are used to show the player how well he did in this run and to encourage him to try again to beat his best score and to increase his total score.
+    /// </summary>
     [SerializeField] Text _bestScore;
+    /// <summary>
+    /// the score of the current run
+    /// </summary>
     [SerializeField] Text _thisRunScore;
+    /// <summary>
+    /// UI Text component that displays the total score.
+    /// </summary>
+    /// <remarks>Assigned in the Unity Inspector via SerializeField. May be null if not assigned at
+    /// runtime.</remarks>
     [SerializeField] Text _totalScore;
-    [SerializeField] SaveableItem _totalScoreSavable;
+
+    /// <summary>
+    /// it is visible only when the player has a new best score, and it is invisible otherwise. it is used to show the player that he has a new best score and to encourage him to try again to beat his best score.
+    /// </summary>
     [SerializeField] GameObject _newBestRecordText;
     //[SerializeField] LootLockerRankingManager _rankingManager;
     public void _Back()
@@ -19,13 +32,11 @@ public class AftermathManager : MonoBehaviour
     IEnumerator Start()
     {
         yield return null;
+
+
         //_rankingManager._SetActiveLeaderBoard((int)A.Levels.DifficultyLevel, A.Levels.ThisRoundScore);
         
-        _totalScoreSavable._ChangeAmount(A.Levels.ThisRoundScore);
-
-        _bestScore.text = A.Tools.ScoreToTitle(A.Levels.BestScore);
-        _thisRunScore.text = A.Tools.ScoreToTitle(A.Levels.ThisRoundScore);
-        _totalScore.text = A.Tools.ScoreToTitle(_totalScoreSavable._Stock);
+        
 
         if (A.Levels.SetBestScore(A.Levels.ThisRoundScore))
         {
@@ -35,6 +46,12 @@ public class AftermathManager : MonoBehaviour
         {
             _newBestRecordText.SetActive(false);
         }
+
+        _bestScore.text = A.Tools.ScoreToTitle(A.Levels.BestScore);
+        _thisRunScore.text = A.Tools.ScoreToTitle(A.Levels.ThisRoundScore);
+        _totalScore.text = A.Tools.ScoreToTitle(A.GameSetting.ScoreTotal._Stock);
+
+        
     }
     //public void _ShowRanks()
     //{
@@ -67,7 +84,24 @@ public class AftermathManager : MonoBehaviour
     public void _TryAgain()
     {
 
-        SoundManager._Instance._PlaySound(GameSounds.ButtomClicked);
         BAHMANLoadingManager._INSTANCE._LoadScene(AllScenes.TitleScreenScene);
+    }
+    public void _ButtonClicked()
+    {
+        SoundManager._Instance._PlaySound(GameSounds.ButtomClicked);
+    }
+
+    public void _ClaimReward()
+    {
+        _claimReward(A.Levels.ThisRoundScore);
+    }
+    public void _ClaimDoubleReward()
+    {
+        _claimReward(A.Levels.ThisRoundScore * 2);
+    }
+    void _claimReward(int iReward)
+    {
+        A.GameSetting.ScoreTotal._ChangeAmount(iReward);
+        _totalScore.text = A.Tools.ScoreToTitle(A.GameSetting.ScoreTotal._Stock);
     }
 }
